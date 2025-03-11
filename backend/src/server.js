934fc02c2564abb44e3c025d18ac1edc1eba28fb
Sofia Neo <sofineo@gmail.com>
2025-03-10 23:52:31 -0700

    Change URL to server ipv4

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,6 @@ const { PORT } = require("./config/envConfig");
 const app = require("./app");
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}/api`);
-  console.log(`Swagger Docs available at http://localhost:${PORT}/api-docs`);
+  console.log(`🚀 Server running on http://3.146.34.160:${PORT}/api`);
+  console.log(`Swagger Docs available at http://3.146.34.160:${PORT}/api-docs`);
 });
